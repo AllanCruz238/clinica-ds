abebe50c17c45fe4a7d90baea530948596fa06c9
AllanCruz238 <acruzp8@miumg.edu.gt>
2026-05-23 04:17:54 -0600

    Corregir migraciones existentes en Render

--- build.sh
+++ build.sh
@@ -3,8 +3,5 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-
-python manage.py migrate core zero --fake || true
-python manage.py migrate
-
+python manage.py migrate --fake-initial
 python seed_admin.py
