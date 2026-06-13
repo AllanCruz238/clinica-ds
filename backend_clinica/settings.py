c0995d6fe885d97c4bf9cdfd5e75f2c415ac5fd9
AllanCruz238 <acruzp8@miumg.edu.gt>
2026-06-13 14:12:28 -0600

    Corregir configuracion de envio de correos

--- backend_clinica/settings.py
+++ backend_clinica/settings.py
@@ -140,3 +140,14 @@ EMAIL_HOST_USER = 'allanjcruzp04@gmail.com'
 EMAIL_HOST_PASSWORD = 'duks idhu xkry wufe'
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', '')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
